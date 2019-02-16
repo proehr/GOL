d251690bd2fe81d36d1a3c34f40397e10e5a9d53
proehr <proehr@users.noreply.github.com>
2019-02-16 01:52:42 +0100

    Startfunction, Stopbutton, etc.

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -18,30 +18,9 @@ public class SampleController implements Initializable {
     public void loadNewScene(ActionEvent actionEvent) throws Exception{
         int height = Integer.parseInt(heightField.getText());
         int width = Integer.parseInt(widthField.getText());
-        GameData.getInstance().setHeight(height);
-        GameData.getInstance().setWidth(width);
+        GameData.getInstance().setData(height, width);
         AnchorPane pane = FXMLLoader.load(getClass().getResource("playfield.fxml"));
-        pane.setAccessibleText(height + " " + width);
         rootAnchor.getChildren().setAll(pane);
-        /*BorderPane borderPane = new BorderPane();
-        GridPane gridPane = new GridPane();
-        TextField ruleField = new TextField();
-        Label ruleLabel = new Label("Rule:");
-        ObservableList<String> options =
-                FXCollections.observableArrayList(
-                        "Dead",
-                        "Alive",
-                        "Go Around"
-                );
-        ComboBox borderBox = new ComboBox(options);
-        Label borderLabel = new Label("Border:");
-        gridPane.add(ruleLabel,0,0);
-        gridPane.add(ruleField,1,0);
-        gridPane.add(borderLabel,0,1);
-        gridPane.add(borderBox, 1,1);
-        borderPane.setTop(gridPane);
-        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-        Scene scene= stage.getScene();*/
 
     }
 
