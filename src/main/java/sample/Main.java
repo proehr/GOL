70d25223dfb5c23a448e49596009b6a24a5d01cf
proehr <47065949+proehr@users.noreply.github.com>
2019-07-31 22:53:08 +0200

    Wanted to make it maven, didnt work

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,18 +1,18 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
+        import javafx.stage.Stage;
 
 
 public class Main extends Application {
-    private Stage stage;
+    private static Stage stage;
     @Override
     public void start(Stage primaryStage) throws Exception{
         stage =primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         Scene scene =new Scene(root, 500, 500);
         stage.setTitle("Game of Life");
         stage.setScene(scene);
@@ -23,5 +23,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 }
